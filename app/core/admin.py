6254f4ae258e8d4a278bd683007f70ba28049a3d
Carlos Eduardo Dominguez Martinez <carlosedm@hotmail.es>
2023-08-16 15:02:39 +0200

    Django Admin set up

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
+"""
+Django admin customization
+"""
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}), # first fieldset
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}), # last fieldset
+    )
+    readonly_fields = ('last_login',) # make last_login read only
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',), # this changes the style of the form
+            'fields': (
+            'email',
+            'password1',
+            'password2',
+            'name',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+                    )
+                }
+        ), # first fieldset
+    )
+
+
+admin.site.register(models.User, UserAdmin)
 
 # Register your models here.
